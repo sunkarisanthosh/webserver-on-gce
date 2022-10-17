4ba82bedfe1a7f59d3c735fcdec0f038181e311c
sunkarisanthosh <77742585+sunkarisanthosh@users.noreply.github.com>
2022-10-17 13:26:23 +0100

    Update logs-to-gcs.tf

--- logs-to-gcs.tf
+++ logs-to-gcs.tf
@@ -1,19 +1,17 @@
 resource "google_storage_bucket" "log-bucket" {
-  name     = "my-unique-logging-bucket-santhosh"
+  name     = "bucket-to-store-webserver-logs"
   location = "EU"
 }
 
-# Our sink; this logs all activity related to our "my-logged-instance" instance
 resource "google_logging_project_sink" "instance-sink" {
   name        = google_compute_instance.vm.name
-  description = "some explanation on what this is"
+  description = "bucket-to-store-webserver-logs"
   destination = "storage.googleapis.com/${google_storage_bucket.log-bucket.name}"
   filter      = "resource.type = gce_instance AND resource.labels.instance_id = \"${google_compute_instance.vm.id}\""
 
   unique_writer_identity = true
 }
 
-# Because our sink uses a unique_writer, we must grant that writer access to the bucket.
 resource "google_project_iam_binding" "log-writer" {
   project = "${var.project_id}"
   role = "roles/storage.objectCreator"
