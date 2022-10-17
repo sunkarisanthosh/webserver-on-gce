9fba8aa503f5c96446b57be126200085643facd2
sunkarisanthosh <77742585+sunkarisanthosh@users.noreply.github.com>
2022-10-17 13:11:08 +0100

    Update compute-engine.tf

--- compute-engine.tf
+++ compute-engine.tf
@@ -41,19 +41,3 @@ resource "google_compute_instance" "vm" {
 
 }
 
-resource "google_compute_disk" "additional-disk-to-webserver" {
-  name = "additional-disk-to-webserver"
-  type = "pd-ssd"
-  zone = "europe-west1-b"
-  size = 4
-  labels = {
-    environment = "dev"
-  }
-  physical_block_size_bytes = 4096
-}
-
-resource "google_compute_attached_disk" "default" {
-  disk     = google_compute_disk.additional-disk-to-webserver.id
-  instance = google_compute_instance.vm.id
-}
-
